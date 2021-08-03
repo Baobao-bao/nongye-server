016ba99597fb33d4ad2e4eb6745eb942bcb47440
huruqing <huruqing@qq.com>
2021-08-03 21:11:53 +0800

    xxx

--- router/modules/countRoute.js
+++ router/modules/countRoute.js
@@ -4,50 +4,50 @@ module.exports = {
   path: "/count",
   name: "count模块",
   children: [
-    {
-      path: "/add",
-      name: "添加",
-      service: count["add"],
-      params: {
-        name: "count"
-      }
-    },
+    // {
+    //   path: "/add",
+    //   name: "添加",
+    //   service: count["add"],
+    //   params: {
+    //     name: "count"
+    //   }
+    // },
     {
       path: "/all",
       name: "全部列表",
       service: count["all"]
     },
-     {
-       path: "/list",
-       name: "列表分页查询",
-       service: count["list"],
-       params: {
-         pageNum: "当前页"
-       }
-     },
-       {
-       path: "/detail",
-       name: "详情",
-       service: count["detail"],
-       params: {
-         id: "id"
-       }
-     },
-     {
-       path: "/edit",
-       name: "修改",
-       service: count["edit"],
-       params: {
-         id: "id"
-       }
-     },
-     {
-       path: "/del",
-       name: "删除",
-       service: count["del"],
-       params: {
-         id: "id"
-       }
-     }
+    //  {
+    //    path: "/list",
+    //    name: "列表分页查询",
+    //    service: count["list"],
+    //    params: {
+    //      pageNum: "当前页"
+    //    }
+    //  },
+    //    {
+    //    path: "/detail",
+    //    name: "详情",
+    //    service: count["detail"],
+    //    params: {
+    //      id: "id"
+    //    }
+    //  },
+    //  {
+    //    path: "/edit",
+    //    name: "修改",
+    //    service: count["edit"],
+    //    params: {
+    //      id: "id"
+    //    }
+    //  },
+    //  {
+    //    path: "/del",
+    //    name: "删除",
+    //    service: count["del"],
+    //    params: {
+    //      id: "id"
+    //    }
+    //  }
   ]
 };
