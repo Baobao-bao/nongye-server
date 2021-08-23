5ed6e43fdcd054a2904ffb127d64dd84e79decae
“huruqing” <huruqing@gmail.com>
2021-08-23 16:39:52 +0800

    xxx

--- db/app.js
+++ db/app.js
@@ -3,7 +3,11 @@ const $db = require(__dirname + "/db.json");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 const router = jsonServer.router($db); 
+const path = require('path');
 
+server.use(jsonServer.defaults({
+    static:path.join(__dirname, '/public'),//静态资源托管
+  }));
 server.use(router);
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
