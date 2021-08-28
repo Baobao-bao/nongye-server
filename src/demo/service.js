825df00346c2376e6126933e3b2f39f70aec33e3
“huruqing” <huruqing@gmail.com>
2021-08-28 17:28:01 +0800

    第21次提交, 备注信息:无

--- src/demo/service.js
+++ src/demo/service.js
@@ -45,7 +45,7 @@
       //     msg: 'success',
       //     result: res.data
       //   };
-      ctx.body = 2222;
+      ctx.body = 11111;
       // } catch (error) {
       //   ctx.body = {
       //     code: 500,
