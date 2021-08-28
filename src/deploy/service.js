e09dbed5c1a8d0b4e46ce1c849c869908e025f33
“huruqing” <huruqing@gmail.com>
2021-08-28 16:16:15 +0800

    xxx

---
+++ src/deploy/service.js
@@ -0,0 +1,106 @@
+
+  const $ = require('axios');
+  const UUID = require("uuid");
+  const Common = require('../common/index');
+  const shell = require('shelljs');
+  class Service extends Common{
+    constructor() {
+      super('Deploy');
+    };
+  
+    async index(ctx) {
+      shell.exec('git checkout .');
+      shell.exec('git pull origin master');
+      // shell.exec('git pull origin master');
+      ctx.body = 2222;
+    }
+  
+    async list(ctx) {  
+      console.log(this.getTotalPage());
+      let {_page,_limit} = ctx.params;
+      let params = {
+        _sort:'updateTime',_order:'desc',
+        _page,
+        _limit
+      }
+      try {
+        let res = await $.get(this.url + '/Deploy',{params});
+        ctx.body = {
+          code: 666,
+          msg: 'success',
+          result: res.data
+        };
+      } catch (error) {
+        ctx.body = {
+          code: 500,
+          msg: error.message
+        }
+      }
+    }
+  
+    async all(ctx) {
+      try {
+        let res = await $.get(this.url + '/Deploy');
+        ctx.body = {
+          code: 666,
+          msg: 'success',
+          result: res.data
+        };
+      } catch (error) {
+        ctx.body = {
+          code: 500,
+          msg: error.message
+        }
+      }
+    }
+  
+    async edit(ctx) {
+      try {
+        ctx.params.updateTime = Date.now();
+        let res = await $.put(this.url + '/Deploy/' + ctx.params.id, ctx.params);
+        ctx.body = {
+          code: 666,
+          msg: 'success',
+          result: res.data
+        }
+      } catch (error) {
+        ctx.body = {
+          code: 500,
+          msg: error.message
+        }
+      }
+    }
+  
+    async detail(ctx) {
+      try {
+        let res = await $.get(this.url + '/Deploy/' + ctx.params.id);
+        ctx.body = {
+          code: 666,
+          msg: 'success',
+          result: res.data
+        }
+      } catch (error) {
+        ctx.body = {
+          code: 500,
+          msg: error.message
+        }
+      }
+    }
+  
+    async del(ctx) {
+      try {
+        let res = await $.delete(this.url + '/Deploy/' + ctx.params.id);
+        ctx.body = ctx.body = {
+          code: 666,
+          msg: 'success',
+          result: res.data
+        };
+      } catch (error) {
+        ctx.body = {
+          code: 500,
+          msg: error.message
+        }
+      }
+    } 
+  }
+  module.exports = new Service();
