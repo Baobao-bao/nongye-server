15938cbe0051227c8c19c8f7bdc51f6eab94a6ea
“huruqing” <huruqing@gmail.com>
2021-08-28 17:51:31 +0800

    第37次提交, 备注信息:无

--- src/deploy/service.js
+++ src/deploy/service.js
@@ -10,7 +10,8 @@
   
     async index(ctx) { 
       shell.exec('git pull origin master');
-      shell.exec('pm2 restart villa2-server');
+      shell.exec('pm2 stop villa2-server');
+      shell.exec('pm2 start villa2-server');
       // shell.exec('pm2 restart villa2-json');
       ctx.body = '部署中........';
     }
