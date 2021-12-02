185add2eddf91dc5b3ff867e9849263007e57c5d
Baobao-bao <baobao5347@gmail.com>
2021-12-02 16:15:52 +0800

    .

--- src/common/index.js
+++ src/common/index.js
@@ -1,153 +1,154 @@
-const {mock} = require('mockjs');
+const { mock } = require("mockjs");
 const jwt = require("jsonwebtoken");
 const $ = require("axios");
 const service = $.create({
-  baseURL: process.env.dbpath,
+    baseURL: process.env.dbpath,
 });
 service.interceptors.response.use(
-  (response) => {
-    return Promise.resolve(response.data);
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    (response) => {
+        return Promise.resolve(response.data);
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
 );
 
 class Utils {
-  constructor(collName) {
-    this.url = process.env.dbpath;
-    this.coll = collName;
-  }
-
-  /**
-   * 分页
-   */
-  pageing(list, currPage, pageSize) {
-    currPage=!!currPage?currPage:1;
-    pageSize=!!pageSize?pageSize:5;
-    return list.slice(
-      (currPage - 1) * Number(pageSize),
-      (currPage - 1) * Number(pageSize) + Number(pageSize)
-    );
-  }
-
-  filterByDate(list, date) {
-    if (!!date) {
-      let index = date.indexOf("-");
-      let date1 = date.slice(0, index);
-      let date2 = date.slice(index + 1);
-      return list.filter((item) => {
-        return item.bTime >= date1 && item.bTime <= date2;
-      });
-    } else {
-      return list;
+    constructor(collName) {
+        this.url = process.env.dbpath;
+        this.coll = collName;
     }
-  }
 
-  /**
-   * 过滤列表
-   * @param {*} obj 要过滤的属性
-   */
-  filterList(list, attrObj) {
-    let newList = list;
-    for (let key in attrObj) {
-      let value = attrObj[key];
-      if (typeof value !== "boolean") {
-        // vale值为undefined或者'all'的不进行过滤
-        if (!!value && value !== "all") {
-          newList = filterFn(key);
+    /**
+     * 分页
+     */
+    pageing(list, currPage, pageSize) {
+        currPage = !!currPage ? currPage : 1;
+        pageSize = !!pageSize ? pageSize : 5;
+        return list.slice((currPage - 1) * Number(pageSize), (currPage - 1) * Number(pageSize) + Number(pageSize));
+    }
+
+    filterByDate(list, date) {
+        if (!!date) {
+            let index = date.indexOf("-");
+            let date1 = date.slice(0, index);
+            let date2 = date.slice(index + 1);
+            return list.filter((item) => {
+                return item.bTime >= date1 && item.bTime <= date2;
+            });
+        } else {
+            return list;
         }
-      } else {
-        // 布尔类型执行判断
-        newList = filterFn(key);
-      }
     }
 
-    function filterFn(key) {
-      // 值带有key
-      if (key.includes("key")) {
-        // 原来的key
-        let originkey = key.slice(0, -3);
-        return newList.filter((item) => item[originkey].includes(attrObj[key]));
-      } else {
-        return newList.filter((item) => item[key] === attrObj[key]);
-      }
+    /**
+     * 过滤列表
+     * @param {*} obj 要过滤的属性
+     */
+    filterList(list, attrObj) {
+        let newList = list;
+        for (let key in attrObj) {
+            let value = attrObj[key];
+            if (typeof value !== "boolean") {
+                // vale值为undefined或者'all'的不进行过滤
+                if (!!value && value !== "all") {
+                    newList = filterFn(key);
+                }
+            } else {
+                // 布尔类型执行判断
+                newList = filterFn(key);
+            }
+        }
+
+        function filterFn(key) {
+            console.log("key", key);
+            // 值带有key
+            if (key.includes("key")) {
+                // 原来的key
+                let originkey = key.slice(0, -3);
+
+                return newList.filter((item) => {
+                    return item[originkey].includes(attrObj[key]);
+                });
+            } else {
+                return newList.filter((item) => item[key] === attrObj[key]);
+            }
+        }
+        return newList;
     }
-    return newList;
-  }
 
-  /**
-   * 添加token
-   * @param {object} userInfo
-   */
-  getToken(userInfo) {
-    let secret = "nongye"; // 指定密钥
-    // 生成token
-    let token = jwt.sign(userInfo, secret, {
-      expiresIn: "1000h",
-    });
-    return token;
-  }
-
-  uuid() {
-    return UUID.v1().replace(/(.*?)-(.*?)-.*/, "$1$2");
-  }
-  // 获取总页数
-  async getTotalPage(query) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let res = await $.get(this.url + `/${this.coll}`, { params: query });
-        resolve(res.data.length);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
-
-  log() {
-    console.info("<----------- " + this.coll + " ----------->");
-    for (let i = 0; i < arguments.length; i++) {
-      console.info(arguments[i]);
+    /**
+     * 添加token
+     * @param {object} userInfo
+     */
+    getToken(userInfo) {
+        let secret = "nongye"; // 指定密钥
+        // 生成token
+        let token = jwt.sign(userInfo, secret, {
+            expiresIn: "1000h",
+        });
+        return token;
     }
-    console.info("<----------- " + this.coll + " ----------->");
-  }
-  $get(url, data) {
-    return service.get(url, { params: data });
-  }
-  $patch() {
-    return service.patch;
-  }
-  $put() {
-    return service.put;
-  }
 
-  $post(url, data) {
-    return service.post(url, data);
-  }
+    uuid() {
+        return UUID.v1().replace(/(.*?)-(.*?)-.*/, "$1$2");
+    }
+    // 获取总页数
+    async getTotalPage(query) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let res = await $.get(this.url + `/${this.coll}`, { params: query });
+                resolve(res.data.length);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
 
-  $insert(url, data) {
-    return service.post(url, data);
-  }
+    log() {
+        console.info("<----------- " + this.coll + " ----------->");
+        for (let i = 0; i < arguments.length; i++) {
+            console.info(arguments[i]);
+        }
+        console.info("<----------- " + this.coll + " ----------->");
+    }
+    $get(url, data) {
+        return service.get(url, { params: data });
+    }
+    $patch() {
+        return service.patch;
+    }
+    $put() {
+        return service.put;
+    }
 
-  $find(url, data = {}) {
-    return service.get(url, { params: data });
-  }
+    $post(url, data) {
+        return service.post(url, data);
+    }
 
-  $findById(url, id) {
-    return service.get(url + "?id=" + id);
-  }
-  $update(url, data) {
-    return service.patch(url + `/${data.id}`, data);
-  }
-  $replace(url, data) {
-    return service.put(url + `/${data.id}`, data);
-  }
-  $del(url, id) {
-    return service.delete(url + `/${id}`);
-  }
-  $delById(url, id) {
-    return service.delete(url + `/${id}`);
-  }
+    $insert(url, data) {
+        return service.post(url, data);
+    }
+
+    $find(url, data = {}) {
+        return service.get(url, { params: data });
+    }
+
+    $findById(url, id) {
+        return service.get(url + "?id=" + id);
+    }
+    $update(url, data) {
+        return service.patch(url + `/${data.id}`, data);
+    }
+    $replace(url, data) {
+        return service.put(url + `/${data.id}`, data);
+    }
+    $del(url, id) {
+        return service.delete(url + `/${id}`);
+    }
+    $delById(url, id) {
+        return service.delete(url + `/${id}`);
+    }
 }
 
 module.exports = Utils;
--- src/group/service.js
+++ src/group/service.js
@@ -2,162 +2,158 @@ const $ = require("axios");
 let Mock = require("mockjs");
 const Common = require("../common/index");
 class Service extends Common {
-  constructor() {
-    super("group");
-  }
-
-  async add(ctx) {
-    let data = ctx.params;
-    try {
-      let cTime = Date.now();
-      let uTime = Date.now();
-      let res = await $.post(this.url + "/group", {
-        id: Mock.mock("@id"),
-        cTime,
-        uTime,
-        ...data,
-      });
-      ctx.body = {
-        code: 666,
-        msg: "success",
-        data: res.data,
-      };
-    } catch (error) {
-      ctx.body = {
-        code: 500,
-        msg: error,
-      };
+    constructor() {
+        super("group");
     }
-  }
 
-  async list(ctx) {
-    let { currPage, pageSize, keyword } = ctx.params;
-    currPage = !!currPage ? currPage : 1;
-    pageSize = !!pageSize ? pageSize : 5;
-    let params = {
-      _sort: "uTime",
-      _order: "desc",
-    };
-    try {
-      let groupRes = await $.get(this.url + "/group");
-      let groupList = groupRes.data;
-      // groupList = groupList.map(item=>item.groupName);
+    async add(ctx) {
+        let data = ctx.params;
+        try {
+            let cTime = Date.now();
+            let uTime = Date.now();
+            let res = await $.post(this.url + "/group", {
+                id: Mock.mock("@id"),
+                cTime,
+                uTime,
+                ...data,
+            });
+            ctx.body = {
+                code: 666,
+                msg: "success",
+                data: res.data,
+            };
+        } catch (error) {
+            ctx.body = {
+                code: 500,
+                msg: error,
+            };
+        }
+    }
 
-      let res = await $.get(this.url + "/sensor", { params });
-      let list = res.data;
-      let newList = groupList.map((item) => {
-        let itemList = list.filter(
-          (subitem) => item.groupName === subitem.devGroup
-        );
-        // let id = Mock.mock("@id");
-        // 传感器数量
-        let nodeNums = itemList.length;
-        // 开发套件
-        let groupNums = 10;
-        // devNums 设备数量
-        let devNums = nodeNums + Mock.mock(/[1-3]/) * 1;
-        // 在线设备数量
-        let onlineNums = itemList.filter((item) => item.online === true).length;
-        let cTime =
-          Mock.mock(/202[01]-0[1-9]-[1-2][1-28]/) +
-          Mock.Random.time(" HH:mm:ss");
-        cTime = new Date(cTime);
-        cTime = cTime.getTime();
-        return {
-          // id,
-          ...item,
-          // groupName,
-          nodeNums,
-          groupNums,
-          devNums,
-          onlineNums,
-          cTime,
+    async list(ctx) {
+        let { currPage, pageSize, keyword } = ctx.params;
+        currPage = !!currPage ? currPage : 1;
+        pageSize = !!pageSize ? pageSize : 5;
+        let params = {
+            _sort: "uTime",
+            _order: "desc",
         };
-      });
-      newList = this.filterList(newList, { groupNamekey: keyword });
-      let total = newList.length;
-      newList = this.pageing(newList, currPage, pageSize);
+        try {
+            let groupRes = await $.get(this.url + "/group");
+            let groupList = groupRes.data;
+            // groupList = groupList.map(item=>item.groupName);
 
-      ctx.body = {
-        code: 666,
-        msg: "success",
-        currPage,
-        pageSize,
-        total,
-        data: newList,
-      };
-      return newList;
-    } catch (error) {
-      ctx.body = {
-        code: 500,
-        msg: error.message,
-      };
-    }
-  }
+            let res = await $.get(this.url + "/sensor", { params });
+            let list = res.data;
+            let newList = groupList.map((item) => {
+                let itemList = list.filter((subitem) => item.groupName === subitem.devGroup);
+                // let id = Mock.mock("@id");
+                // 传感器数量
+                let nodeNums = itemList.length;
+                // 开发套件
+                let groupNums = 10;
+                // devNums 设备数量
+                let devNums = nodeNums + Mock.mock(/[1-3]/) * 1;
+                // 在线设备数量
+                let onlineNums = itemList.filter((item) => item.online === true).length;
+                let cTime = Mock.mock(/202[01]-0[1-9]-[1-2][1-28]/) + Mock.Random.time(" HH:mm:ss");
+                cTime = new Date(cTime);
+                cTime = cTime.getTime();
+                return {
+                    // id,
+                    ...item,
+                    // groupName,
+                    nodeNums,
+                    groupNums,
+                    devNums,
+                    onlineNums,
+                    cTime,
+                };
+            });
+            newList = this.filterList(newList, { groupNamekey: keyword });
+            let total = newList.length;
+            newList = this.pageing(newList, currPage, pageSize);
 
-  async all(ctx) {
-    try {
-      let res = await $.get(this.url + "/group");
-      ctx.body = {
-        code: 666,
-        msg: "success",
-        data: res.data,
-      };
-    } catch (error) {
-      ctx.body = {
-        code: 500,
-        msg: error.message,
-      };
+            ctx.body = {
+                code: 666,
+                msg: "success",
+                currPage,
+                pageSize,
+                total,
+                data: newList,
+            };
+            return newList;
+        } catch (error) {
+            ctx.body = {
+                code: 500,
+                msg: error.message,
+            };
+        }
     }
-  }
 
-  async edit(ctx) {
-    try {
-      ctx.params.uTime = Date.now();
-      let res = await $.put(this.url + "/group/" + ctx.params.id, ctx.params);
-      ctx.body = {
-        code: 666,
-        msg: "success",
-        data: res.data,
-      };
-    } catch (error) {
-      ctx.body = {
-        code: 500,
-        msg: error.message,
-      };
+    async all(ctx) {
+        try {
+            let res = await $.get(this.url + "/group");
+            ctx.body = {
+                code: 666,
+                msg: "success",
+                data: res.data,
+            };
+        } catch (error) {
+            ctx.body = {
+                code: 500,
+                msg: error.message,
+            };
+        }
     }
-  }
 
-  async detail(ctx) {
-    try {
-      let res = await $.get(this.url + "/group/" + ctx.params.id);
-      ctx.body = {
-        code: 666,
-        msg: "success",
-        data: res.data,
-      };
-    } catch (error) {
-      ctx.body = {
-        code: 500,
-        msg: error.message,
-      };
+    async edit(ctx) {
+        try {
+            ctx.params.uTime = Date.now();
+            let res = await $.put(this.url + "/group/" + ctx.params.id, ctx.params);
+            ctx.body = {
+                code: 666,
+                msg: "success",
+                data: res.data,
+            };
+        } catch (error) {
+            ctx.body = {
+                code: 500,
+                msg: error.message,
+            };
+        }
     }
-  }
 
-  async del(ctx) {
-    try {
-      let res = await $.delete(this.url + "/group/" + ctx.params.id);
-      ctx.body = ctx.body = {
-        code: 666,
-        msg: "success",
-        data: res.data,
-      };
-    } catch (error) {
-      ctx.body = {
-        code: 500,
-        msg: error.message,
-      };
+    async detail(ctx) {
+        try {
+            let res = await $.get(this.url + "/group/" + ctx.params.id);
+            ctx.body = {
+                code: 666,
+                msg: "success",
+                data: res.data,
+            };
+        } catch (error) {
+            ctx.body = {
+                code: 500,
+                msg: error.message,
+            };
+        }
     }
-  }
+
+    async del(ctx) {
+        try {
+            let res = await $.delete(this.url + "/group/" + ctx.params.id);
+            ctx.body = ctx.body = {
+                code: 666,
+                msg: "success",
+                data: res.data,
+            };
+        } catch (error) {
+            ctx.body = {
+                code: 500,
+                msg: error.message,
+            };
+        }
+    }
 }
 module.exports = new Service();
